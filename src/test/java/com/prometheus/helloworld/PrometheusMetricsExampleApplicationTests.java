c4afbbc911d5f2370b644acf9dfbde68e5f70adf
Chinmay Kulkarni <30969403+Grandolf49@users.noreply.github.com>
2020-04-25 21:59:57 +0530

    metrics exported

---
+++ src/test/java/com/prometheus/helloworld/PrometheusMetricsExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prometheus.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrometheusMetricsExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
